fb8ad1e3f3bfcd70c21958edb51c129c36ee9147
Maelstromage <Reverse91@yahoo.com>
2019-06-24 15:11:06 -0400

    Update JavaUpdate.ps1

--- JavaUpdate.ps1
+++ JavaUpdate.ps1
@@ -1,13 +1,57 @@
+Function Get-JavaURL {
+    Param (
+        $java,
+        $JavaType
+    )
+    $linkGet = $java.links | Where-Object {$_.title -eq $javaType} | Select-Object -Property href
+    $r = $linkGet[0] | Format-Wide -AutoSize | Out-String
+   
+    Return $r.trim()
+    
+}
+Function Get-JavaBundleName {
+    Param(
+    $URL
+    )
+    $r = $URL.split("=")
+    Return $r[1]
+}
+Function Get-JavaDownload {
+    Param(
+    $javaPath,
+    $javaBit,
+    $rootDir,
+    $bit
+    )
+    if (!([System.IO.File]::Exists($javaPath))){
+        Invoke-WebRequest -uri $javaBit -OutFile $JavaPath
+        $version = (get-item $JavaPath).VersionInfo.fileversion | Out-String
+        $version = "java$bit." + $version.trim() + ".exe"
+        Copy-Item $JavaPath "$rootDir\Javatest\$version"
+    }else{Write-Output "$javaPath already Exists!" }
+
+}
+
 
 $java = (Invoke-WebRequest -uri https://java.com/en/download/manual.jsp)
+$rootDir = "c:"
 
-$href64 = $java.links | Where-Object {$_.title -eq "Download Java software for windows (64-bit)"} | Select-Object -Property href
-$Java64 = $href64[0] | Format-Wide -AutoSize | Out-String
-$href32 = $java.links | Where-Object {$_.title -eq "Download Java software for windows Offline"} | Select-Object -Property href
-$Java32 = $href32[0] | Format-Wide -AutoSize | Out-String 
 
-Invoke-WebRequest -uri $java32 -OutFile C:"\java32.exe"
-$version32 = (get-item c:\java.exe).VersionInfo.fileversion | Out-String
-$version32 = "java" + $version32.trim() + ".exe"
+$Java64Bit = Get-JavaURL -java $java -JavaType "Download Java software for windows (64-bit)"
+$Java32Bit = Get-JavaURL -java $java -JavaType "Download Java software for windows Offline"
 
-Rename-Item c:\java.exe -NewName $version32
+
+$java64Bundle = (Get-JavaBundleName -URL $Java64)
+$java32Bundle = (Get-JavaBundleName -URL $Java32)
+
+
+$Java32Path = "$rootDir\java32.$java32Bundle.exe"
+$Java64Path = "$rootDir\java64.$java64Bundle.exe"
+
+Get-JavaDownload -javaPath $Java32Path -javaBit $Java32Bit -rootDir $rootDir -bit "32"
+Get-JavaDownload -javaPath $Java64Path -javaBit $Java64Bit -rootDir $rootDir -bit "64"
+
+
+
+
+  
